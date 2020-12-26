94af0ab0c2b7b6db1a4cc3e6908081bafd02531d
Bakdauley <bakosh21345@gmail.com>
2020-12-27 02:38:31 +0600

    version3

--- settings/production.py
+++ settings/production.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'jo!_#l$e*6s-a*(37*%_)hgxo)j!)rbfz$l0^ii6@^!589dqss'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
