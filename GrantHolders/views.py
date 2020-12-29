a9590d87e1a0b3923b53098e2dd823bd404bba0f
Bakdauley <bakosh21345@gmail.com>
2020-12-29 15:44:04 +0600

    Like system full integraded

--- GrantHolders/views.py
+++ GrantHolders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import GrantHolders,Like
 from django.contrib.auth.models import User
 from .forms import LikeForm
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
 
@@ -22,6 +23,7 @@ def GDetailview(request,url):
     isLiked = None
     if request.user.is_authenticated:
         try:
+            isLiked = None
             isLiked = Like.objects.get(user=request.user,post=grantHolders)
         except Like.DoesNotExist:
             isLiked = None
@@ -33,12 +35,27 @@ def GDetailview(request,url):
 @login_required(login_url='home')
 def like(request,url):
     if request.method == 'POST':
+        data = request.POST
         user = request.user
+        url = data.get('url')
+        grantHolders = GrantHolders.objects.get(slug=url)
+        isLiked = None
         if user.is_authenticated:
-            post = GrantHolders.objects.get(slug=url)
-            like = Like.objects.create(user=user,post=post)
+            try:
+                isLiked = Like.objects.get(user=user,post=grantHolders)
+            except Like.DoesNotExist:
+                isLiked = None
+            
+            if isLiked:
+                pass
+            else:
+                like = Like.objects.create(user=user,post=grantHolders)
+            
+            
             return redirect('detailview',url)
         else:
             pass
-           
+    # if request.method == 'POST':
+    #     print(request.POST)
+    # return request.POST
         
